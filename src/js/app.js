accb23751394e9dec12b4444471a0c4906ed45b5
Hwor001 <128637569+Hwor001@users.noreply.github.com>
2023-05-02 09:45:05 +0300

    lesson 11

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+
+const swiper = new Swiper('.swiper', {
+   direction: 'horizontal',
+   loop: true,
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   slidesPerView: 2,
+ });
+
+ const menu = document.querySelector('.header-navigation')
+ const menuBtn = document.querySelector('.menu-bunger')
+
+if (menu && menuBtn) {
+	menuBtn.addEventListener('click', e => {
+		menu.classList.toggle('active')
+		menuBtn.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+
+
+}
