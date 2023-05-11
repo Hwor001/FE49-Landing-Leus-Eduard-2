ea69f9658895ba3f1feebc13a1d07070fac2c86f
Hwor001 <128637569+Hwor001@users.noreply.github.com>
2023-05-11 07:05:13 +0300

    lesson 12

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,16 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
    loop: true,
    pagination: {
-     el: '.swiper-pagination',
-     clickable: true,
+    el: '.swiper-pagination',
+    clickable: true,
    },
    navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    nextSlide: 3,
+    prevSlide: 3,
    },
-   slidesPerView: 2,
+   slidesPerView: 1,
  });
  
  
